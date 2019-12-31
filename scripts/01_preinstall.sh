4512306bc1575cb416efc7399a92fe4cdae7f4e2
Phu Tran <22720475+phudtran@users.noreply.github.com>
2019-12-30 23:25:27 -0800

    more testing 4

--- scripts/01_preinstall.sh
+++ scripts/01_preinstall.sh
@@ -19,6 +19,3 @@ sudo systemctl start docker
 sudo systemctl enable docker
 
 sudo pkill -9 transitd
-rm -rf /home/ubuntu/mizar
-
-git clone --recurse-submodules -j8 https://github.com/futurewei-cloud/mizar.git /home/ubuntu/mizar
