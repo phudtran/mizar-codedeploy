cfbc666d7fee1d528387b05dc3a4f61d6440cbf0
Phu Tran <22720475+phudtran@users.noreply.github.com>
2020-01-02 15:30:24 -0800

    more testing 14

--- scripts/02_install.sh
+++ scripts/02_install.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-rm -rf /home/ubuntu/mizar2
-git clone --recurse-submodules -j8 https://github.com/phudtran/mizar-codedeploy.git /home/ubuntu/mizar2
+rm -rf $PWD/*
+git clone --recurse-submodules -j8 https://github.com/phudtran/mizar-codedeploy.git .
 
-
-make -C /home/ubuntu/mizar2
-make install -C /home/ubuntu/mizar2
+make -C
+make install -C
